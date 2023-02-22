393a43c9cd784d3e9535f615d2560f47f5ef7721
Vitalii Olkin <volk.in.it@gmail.com>
2023-02-23 02:20:55 +0300

    UR: Add ConfigurationMissingException

---
+++ src/UrlRedirector/UrlRedirector.Webhost/Common/Exceptions/ConfigurationMissingException.cs
@@ -0,0 +1,9 @@
+﻿
+using System.Configuration;
+
+namespace Volkin.UrlRedirector.Webhost.Common.Exceptions
+{
+    [Serializable]
+    public class ConfigurationMissingException : ConfigurationErrorsException
+    { }
+}
